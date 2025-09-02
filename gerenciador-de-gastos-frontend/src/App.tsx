a68a592854dc1024194095a8815bae640156f16d
brunoassuncao15 <105079182+brunoassuncao15@users.noreply.github.com>
2025-09-02 19:01:19 -0300

    cadastro gerenciamento

---
+++ gerenciador-de-gastos-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import CadastroForm from "./pages/CadastroForm";
+import "./pages/CadastroForm.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CadastroForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ gerenciador-de-gastos-frontend/src/pages/CadastroForm.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+const CadastroForm = () => {
+  const [formData, setFormData] = useState({
+    nome: "",
+    email: "",
+    senha: "",
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3001/api/cadastrar", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert(data.mensagem);
+        setFormData({
+          nome: "",
+          email: "",
+          senha: "",
+        });
+      } else {
+        alert("Erro: " + data.mensagem);
+      }
+    } catch (error) {
+      console.error("Erro ao enviar dados:", error);
+      alert("Ocorreu um erro ao tentar se conectar com o servidor.");
+    }
+  };
+
+  return (
+    <div className="cadastro-container">
+      <h2>Tela de Cadastro</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="nome">Nome</label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            value={formData.nome}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="senha">Senha</label>
+          <input
+            type="password"
+            id="senha"
+            name="senha"
+            value={formData.senha}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default CadastroForm;
