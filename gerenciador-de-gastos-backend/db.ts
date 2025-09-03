84c2cea662f0c91c6685d62e149f1146cba87fa2
Bruno <bruno.pereira.5.bp42@gmail.com>
2025-09-02 21:15:43 -0300

    refactor: uncomment database connection logic in db.ts

--- gerenciador-de-gastos-backend/db.ts
+++ gerenciador-de-gastos-backend/db.ts
@@ -16,20 +16,20 @@ const dbConfig = {
 
 const pool = mysql.createPool(dbConfig);
 
-// pool
-//   .getConnection()
-//   .then((connection) => {
-//     console.log(
-//       `Conexão MySQL OK em ${dbConfig.host}:${dbConfig.port} como usuário ${dbConfig.user}`
-//     );
-//     connection.release();
-//   })
-//   .catch((err) => {
-//     console.error("Erro ao conectar ao banco de dados:", err?.message || err);
-//     console.error(
-//       "Verifique as variáveis de ambiente DB_HOST, DB_USER, DB_PASSWORD, DB_NAME e privilégios do usuário."
-//     );
-//     process.exit(1);
-//   });
+pool
+  .getConnection()
+  .then((connection) => {
+    console.log(
+      `Conexão MySQL OK em ${dbConfig.host}:${dbConfig.port} como usuário ${dbConfig.user}`
+    );
+    connection.release();
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao banco de dados:", err?.message || err);
+    console.error(
+      "Verifique as variáveis de ambiente DB_HOST, DB_USER, DB_PASSWORD, DB_NAME e privilégios do usuário."
+    );
+    process.exit(1);
+  });
 
 export default pool;
